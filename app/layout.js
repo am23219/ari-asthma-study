8d840433476f7f676d74d2c0270333e65d6b5114
aMorani <affanalidawah@gmail.com>
2025-05-01 16:03:17 -0400

    removed gtag

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <GoogleTagManager gtmId="GTM-ML5FRTWF" />
+      <GoogleTagManager gtmId="" />
         <link 
           rel="stylesheet" 
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
